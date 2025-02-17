c310cacb94078ac3b655292ab52c31d7007bd7da
Devita Dwi Lestari <devitadwilestari2@gmail.com>
2025-02-17 14:27:25 +0700

    Basic Routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,47 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/hello', function () {
+    return 'Hello World';
+ });
+
+ Route::get('/world', function () {
+    return 'World';
+ });
+ 
+ Route::get('/welcome', function () {
+    return 'Selamat Datang';
+ });
+
+ Route::get('/about', function () {
+    return 'Nama: Devita Dwi Lestari <br>
+    NIM: 2341760002';
+ });
+
+ Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+    });
+    
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+    });
+
+Route::get('/user/{name?}', function ($name='John') {
+   return 'Nama saya '.$name;
+        });
+        
+Route::get('/hello', [WelcomeController::class,'hello']);
 
 /*
 |--------------------------------------------------------------------------
